48af15506d57abbc508a8bd4c8a5b01da89ff7ff
Chelsea Lam <chelsea.lam01@student.csulb.edu>
2022-11-15 20:48:11 -0800

    first release

---
+++ scripts/script.sql
@@ -0,0 +1,26 @@
+-- Drop the table
+DROP TABLE PRODUCT;
+
+-- Create table
+CREATE TABLE PRODUCT (
+   prod_id    	SERIAL PRIMARY KEY,
+   prod_name  	VARCHAR(20) NOT NULL,
+   prod_desc 	VARCHAR(50),
+   prod_price  NUMERIC
+);
+
+-- Insert records
+INSERT INTO PRODUCT (prod_id, prod_name, prod_desc, prod_price)
+VALUES 
+ ('101', 'Pen', 'Black Ink', '0.99'),
+ ('102', 'Pencil', 'Yellow No. 2', '0.35'),
+ ('103', 'Paper', 'College Rule', '1.10'),
+ ('104', 'Eraser', 'Pink', '0.75'),
+ ('105', 'Calculator', 'Scientific', '99.99'),
+ ('106', 'Camera', 'Desktop 720p', '39.99'),
+ ('107', 'Ruler', '12"', '4.75'),
+ ('108', 'Stamps', 'Dozen', '6.60'),
+ ('109', 'Notepad', '8.5 by 11', '2.95'),
+ ('110', 'Charger', 'USB', '22.50');
+
+CRUNCHY_DATABASE_URL="postgres://abcdef123456789:12345678901234567890c674eb05bc549afe83456689206d86719dda70ccb8b6@ec2-52-200-134-180.db.postgresbridge.com:5432/postgres"  
